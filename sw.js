e549145e4c261c3e2f1132a4f0209ac50ed8fee2
BoscoYee <boscokwok7@gmail.com>
2026-09-03 18:45:22 +0800

    updated 3.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hk-gym-pwa-v39";
+const CACHE_NAME = "hk-gym-pwa-v40";
 const CORE_ASSETS = [
   "./",
   "./index.html",
@@ -17,10 +17,10 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    self.clients.claim().then(() => 
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    self.clients.claim().then(() =>
+      caches.keys().then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+      )
     )
   );
 });
@@ -29,21 +29,12 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request).catch(() => caches.match("./index.html"));
-    })
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
   );
 });
-
-
-
-
-
-
-
-
-
-
-
-
